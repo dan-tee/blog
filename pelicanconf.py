d0287a0f1e5f9d788b1756a9e31e981801341f99
daniel <git@dan-t.de>
2016-02-23 20:44:51 +0100

    first version using liquid tags for notebooks

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,7 +8,7 @@ SITESUBTITLE = 'a practitioners view on data science'
 SITELOGO = '/images/black_swan.jpg'
 FAVICON_FILENAME = '/images/black_swan.jpg'
 
-#MARKUP = ('md', 'ipynb')
+MARKUP = ('md')
 THEME = 'modernscientist-theme'
 SEARCH_BOX = True
 X_MIN_READ = True
@@ -20,10 +20,10 @@ PATH = 'content'
 TIMEZONE = 'Europe/Berlin'
 DEFAULT_DATE_FORMAT = '%d %b %Y'
 DEFAULT_LANG = 'en'
-TIMEZONE = 'Europe/Berlin'
 
 PLUGIN_PATHS = ['../pelican-plugins', '../pelican-ipynb']
-PLUGINS = ['post_stats', 'ipynb']
+PLUGINS = ['post_stats', 'liquid_tags.notebook', 'liquid_tags.img']
+EXTRA_HEADER = open('_nb_header.html').read()
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -43,7 +43,7 @@ LINKS = (('Edwin Chen', 'http://blog.echen.me/'),
 #TWITTER_USER = 'Dan__Tee'
 
 # Social widget
-SOCIAL = (('Twitter', 'https://twitter.com/swanint'),)
+SOCIAL = (('Twitter', 'http://twitter.com/swanint'),)
 
 DEFAULT_PAGINATION = 25
 TYPOGRIFY = False
