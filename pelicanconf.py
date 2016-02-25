9406b8d478213b02d9811962c60faea9d401f3f4
daniel <git@dan-t.de>
2016-02-25 22:28:19 +0100

    first embedded video

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,12 +17,14 @@ MENUITEMS = [('Latest', '/index.html'),
              ('All', '/archives.html'),]
 
 PATH = 'content'
+STATIC_PATHS= ['images', 'videos']
+USE_FOLDER_AS_CATEGORY = False
 TIMEZONE = 'Europe/Berlin'
 DEFAULT_DATE_FORMAT = '%d %b %Y'
 DEFAULT_LANG = 'en'
 
 PLUGIN_PATHS = ['../pelican-plugins', '../pelican-ipynb']
-PLUGINS = ['post_stats', 'liquid_tags.notebook', 'liquid_tags.img']
+PLUGINS = ['post_stats', 'liquid_tags.notebook', 'liquid_tags.img', 'liquid_tags.video']
 EXTRA_HEADER = open('_nb_header.html').read()
 
 # Feed generation is usually not desired when developing
